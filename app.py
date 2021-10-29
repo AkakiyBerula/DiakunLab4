4f3e44240e9228d3ddeff25021d42d0c047fc786
AkakiyBerula <vlad5dyakun@gmail.com>
2021-10-29 13:56:59 +0300

    Step 7

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length, AnyOf
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Thisisasecret!"
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired('A username is required!')])
-    password = PasswordField('password', validators=[InputRequired('Password is required!')])
+    username = StringField('username', validators=[InputRequired('A username is required!'), Length(min=5, max=10, message='Must be from 5 to 10 symbols')])
+    password = PasswordField('password', validators=[InputRequired('Password is required!'), AnyOf(values=['password', 'secret'])])
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
