5b2b41f80fe378d59cdf2903abc88c68e5c7aea1
AkakiyBerula <vlad5dyakun@gmail.com>
2021-10-29 06:07:56 +0300

    Step 2

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Thisisasecret!"
 
+class LoginForm(FlaskForm):
+    username = StringField('username')
+    password = PasswordField('password')
+
 @app.route('/form')
 def form():
-    return render_template('form.html')
+    form = LoginForm()
+    return render_template('form.html', form=form)
 
 if __name__ == '__name__':
     app.run(debug=True)
