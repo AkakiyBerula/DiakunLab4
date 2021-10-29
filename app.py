2f13dd034e7eefa248e4d6a9394416b2d79ef6a0
AkakiyBerula <vlad5dyakun@gmail.com>
2021-10-29 06:17:40 +0300

    Step 3

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ class LoginForm(FlaskForm):
     username = StringField('username')
     password = PasswordField('password')
 
-@app.route('/form')
+@app.route('/form', methods=['GET', 'POST'])
 def form():
     form = LoginForm()
+    if form.validate_on_submit():
+        return '<h1>The username is {}. The password is {}.</h1>'.format(form.username.data, form.password.data)
     return render_template('form.html', form=form)
 
 if __name__ == '__name__':
